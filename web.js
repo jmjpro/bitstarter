13fd607de9f6dee94f4367e9cef41f1683e1cbbe
Joshua Jacobson <jmjpro@gmail.com>
2013-07-11 03:42:20 +0000

    read response from file

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+//we could also read this directly into a string and avoid the buffer
+var inputFileBuf = fs.readFileSync('index.html');
+var inputString = inputFileBuf.toString('utf-8');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(inputString);
 });
 
 var port = process.env.PORT || 5000;
